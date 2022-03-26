6ccf358e7836881062fd8f6a63f11c1f437f5001
ViniciusPolo <admviniciuspolo@gmail.com>
2022-03-26 11:04:02 -0300

    (26/06) Conexão no banco de dados parametrizada com o dotenv

--- back-end/app.js
+++ back-end/app.js
@@ -1,3 +1,9 @@
+// Carrega as variáveis de ambiente do arquivo .env
+require('dotenv').config()
+
+//exibindo as variáveis de ambiente no console (teste)
+console.log(process.env)
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,6 +13,9 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+const dbConnection = require('./config/database')
+dbConnection()
 
 app.use(logger('dev'));
 app.use(express.json());
