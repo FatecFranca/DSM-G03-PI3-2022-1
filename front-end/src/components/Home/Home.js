9fbf14e23017a4ed206c7a2f9d8265a2fc7a1185
ViniciusPolo <admviniciuspolo@gmail.com>
2022-06-02 21:45:36 -0300

    02-06 - getAssessment

--- front-end/src/components/Home/Home.js
+++ front-end/src/components/Home/Home.js
@@ -12,7 +12,15 @@ function Home() {
       <Header />
       <Menu />
       <main className="content">
-          <h1>Bem Vindo ao UX Helpers</h1> 
+          <h1>Bem Vindo de volta ao UX Helpers</h1>
+          <p>Ultimas notícias sobre Tecnologia, ultimas tendencias de design e UX/UI</p>
+          <div className="page-principal">
+            <ul>
+            <p>
+              Alunos de faculdade publica, recria projeto que realiza avaliações de paginas web.
+            </p>
+            </ul>
+          </div>
       </main>
       <Footer />
     </div>
