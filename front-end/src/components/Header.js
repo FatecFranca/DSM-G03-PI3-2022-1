335044140df76e29420664ea72fbfc2f5f5b2b66
Fabrício Rangel de Sousa <fabricio3176@gmail.com>
2022-06-05 13:49:30 -0300

    inclusao botao pesquisar
    inclusao do botao pesquisar

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -7,7 +7,9 @@ function Header() {
             <Link to="/" class="botaoSair">Sair</Link>
             <div className="titulo-pesquisa">
                 <h2 id="title">UX Helpers</h2>
+                <i class="fa fa-times" id="clear-val" aria-hidden="true"></i>
                 <input id="pesquisa"type="text" name="Pesquisar" placeholder="Pesquisar"></input>
+                <button class="btn" type="submit">Pesquisar</button>   
             </div>
         </div>
     )
