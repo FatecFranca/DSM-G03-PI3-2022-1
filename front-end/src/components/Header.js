c143a8b3dff75616b74f2c047a9be15af4f4079a
Fabrício Rangel de Sousa <fabricio3176@gmail.com>
2022-06-06 20:55:17 -0300

    Update Header.js
    Correção dos codigos e iniciar novamente apos o erro.

--- front-end/src/components/Header.js
+++ front-end/src/components/Header.js
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
 import { Link } from 'react-router-dom'
+import { BiSearch } from 'react-icons/bi';
 
 function Header() {
     return (
@@ -7,9 +7,14 @@ function Header() {
             <Link to="/" class="botaoSair">Sair</Link>
             <div className="titulo-pesquisa">
                 <h2 id="title">UX Helpers</h2>
-                <input value="Pesquisar"></input>
+                <form>
+                    <BiSearch class="icon" />
+                    <input class="pesquisa" type="text" placeholder="Pesquisar..." />
+                    <button class="btn" type="submit">Buscar</button>
+                </form>
             </div>
         </div>
     )
 }
+
 export default Header
