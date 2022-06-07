132ba126a86b8fa5a70097422f52fe456b1020d3
ViniciusPolo <admviniciuspolo@gmail.com>
2022-06-07 20:48:17 -0300

    07-06 modificações componentes

--- front-end/src/components/Menu.js
+++ front-end/src/components/Menu.js
@@ -8,7 +8,6 @@ function Menu() {
                 <li><Link to="/home" class="menuitem">Principal</Link></li>
                 <li><Link to="/avaliacoes" class="menuitem">Avaliações</Link></li>
                 <li><Link to="/questoes" class="menuitem">Questões</Link></li>
-                <li><Link to="/respostas" class="menuitem">Respostas</Link></li>
                 <li><Link to="/glossario" class="menuitem">Glossário</Link></li>
             </ul>
         </aside>
